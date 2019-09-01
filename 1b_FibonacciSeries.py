874bce9f9035c2018c73b90193072df1d3366759
TejashwiniV <51438156+TejashwiniV@users.noreply.github.com>
2019-09-01 17:13:08 +0530

    Update and rename fibonacci_series to 1b_FibonacciSeries.py

---
+++ 1b_FibonacciSeries.py
@@ -0,0 +1,8 @@
+def fib(num):
+    if num <= 1:
+        return num
+    else:
+        return(fib(num-1)+fib(num-2))
+num = int(input("Enter the number to generate fibonacci series:"))
+for i in range(num):
+    print(f'Fibonacci{[i]}:{fib(i)}')
