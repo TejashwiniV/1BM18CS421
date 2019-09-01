08475856826095193bb22827094771554b84a024
TejashwiniV <51438156+TejashwiniV@users.noreply.github.com>
2019-09-01 17:51:10 +0530

    Update 1b_FibonacciSeries.py

--- 1b_FibonacciSeries.py
+++ 1b_FibonacciSeries.py
@@ -3,6 +3,7 @@ def fib(num):
         return num
     else:
         return(fib(num-1)+fib(num-2))
+    
 num = int(input("Enter the number to generate fibonacci series:"))
 for i in range(num):
     print(f'Fibonacci{[i]}:{fib(i)}')
