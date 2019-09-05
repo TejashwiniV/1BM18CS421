a6e65e856afbfc098333c339bb4beaa59d8f0706
TejashwiniV <51438156+TejashwiniV@users.noreply.github.com>
2019-09-05 12:35:17 +0530

    Update 3b_PasswordGenerator.py

--- 3b_PasswordGenerator.py
+++ 3b_PasswordGenerator.py
@@ -1,15 +1,17 @@
 import string
 import random
 
-def func(len):
-    upper = [random.choice(string.ascii_uppercase) for upper in range(len)]
-    lower = [random.choice(string.ascii_lowercase) for lower in range(len)]
-    digits = [random.choice(string.digits) for dig in range(len)]
-    special = [random.choice(string.punctuation) for spl in range(len)]
-    merge = "".join(upper + lower + digits + special)
-    generator = "".join(random.choice(merge) for i in range(len))
+def func(words):
+    upper_case = [random.choice(string.ascii_uppercase) for upper in range(words)]
+    lower_case = [random.choice(string.ascii_lowercase) for lower in range(words)]
+    digits = [random.choice(string.digits) for dig in range(words)]
+    special_characters = [random.choice(string.punctuation) for spl in range(words)]
+    pwd = "".join(upper_case + lower_case + digits + special_characters)
+    print("Merged string: ",pwd)
+    print("Length of merged string: ",len(pwd))
+    generator = ''.join(random.choice(pwd) for i in range(words))
     return generator
     
-len = int(input("Enter the length of the password to generate: "))
-password = func(len)
+words = int(input("Enter the length of the password to generate: "))
+password = func(words)
 print("Password:",password)
